a526b21303d6b6db63d958a5e739167bf54acbaf
Masataro Asai <guicho2.71828@gmail.com>
2016-03-30 14:51:25 +0900

    deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+
+[ -d _deploy ] || git clone . _deploy
+
+pushd _deploy
+git pull
+
+head=$(git rev-parse HEAD)
+
+git checkout --detach
+make
+git add -f --all
+git commit -m "Deployed $(date)"
+git tag deploy-$(date +%Y-%m-%d-%H-%M)
+git push -f $1 HEAD:refs/heads/gh-pages
+
+trap "git reset $head" EXIT
+trap "popd; rm -r _deploy" INT
