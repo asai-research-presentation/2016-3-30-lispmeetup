8922619baeca8b82c628c7c23a53abfee6572696
Masataro Asai <guicho2.71828@gmail.com>
2016-03-31 11:22:47 +0900

    do not deploy org-mode and htmlize source

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,6 +10,7 @@ head=$(git rev-parse HEAD)
 git checkout --detach
 make
 git add -f --all
+git reset HEAD -- org-mode htmlize
 git commit -m "Deployed $(date)"
 git tag deploy-$(date +%Y-%m-%d-%H-%M)
 git push -f $1 HEAD:refs/heads/gh-pages
